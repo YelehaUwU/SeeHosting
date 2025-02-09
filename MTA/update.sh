157dc4452a31fa44a536d3e5f5663a0af47fc143
YelehaUwU <162886331+YelehaUwU@users.noreply.github.com>
2025-02-09 14:38:51 +0100

    Update update.sh

--- MTA/update.sh
+++ MTA/update.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-SERVER_DIRECTORY="/home/container"
+if [[ "$AUTO_UPDATE" != "1" ]]; then
+    echo "Auto-update is disabled. Enable AUTO UPDATE for automatic updates."
+    exit 0
+fi
 
+SERVER_DIRECTORY="/home/container"
 if [[ -d /mnt/server ]]; then
     SERVER_DIRECTORY="/mnt/server"
 fi
 
 WEBPAGE_URL="https://nightly.multitheftauto.com/"
-
 webpage_content=$(curl -s "$WEBPAGE_URL")
-
 download_link=$(echo "$webpage_content" | grep -oP 'href="(multitheftauto_linux_x64-1\.6\.0-rc-\d+\.tar\.gz)"' | sed 's/href="//;s/"//' | head -n 1)
 
 if [[ -z "$download_link" ]]; then
@@ -17,16 +19,14 @@ if [[ -z "$download_link" ]]; then
 else
     full_download_link="https://nightly.multitheftauto.com/$download_link"
     echo "Download link: $full_download_link"
-
+    
     curl -O "$full_download_link"
-
     filename=$(basename "$full_download_link")
-
+    
     if [[ ! -f "$filename" ]]; then
         echo "Download failed. Skipping update."
     else
         tar -xvzf "$filename" --strip-components=1 -C "$SERVER_DIRECTORY"
-
         if [[ $? -ne 0 ]]; then
             echo "Extraction failed. Skipping update."
         else
